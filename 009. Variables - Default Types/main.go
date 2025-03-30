7ea120dad7cd339362c700e0fed102df15eac35d
AbhinandAmmanamkandy <abhinandammanamkandypvt@gmail.com>
2025-03-31 01:12:04 +0530

    Variables - Default Types

---
+++ 009. Variables - Default Types/main.go
@@ -0,0 +1,18 @@
+package main
+
+func main() {
+	// Always prefer default types
+
+	// Default types
+	// bool
+	// string
+	// int
+	// uint
+	// byte
+	// rune
+	// float64
+	// complex128
+
+	// When to not use default type
+	// When performace and memory are the primary concern
+}

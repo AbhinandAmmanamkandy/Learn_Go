8e7d715a02f52a42818101a8cb7fdbc209c776ab
AbhinandAmmanamkandy <abhinandammanamkandypvt@gmail.com>
2025-03-30 23:26:01 +0530

    Variables - Short Variable Declaration

---
+++ 003. Variables - Short Variable Declaration/main.go
@@ -0,0 +1,37 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// sad variable declaration
+	var mySkillIssues int
+	mySkillIssues = 42
+	fmt.Println(mySkillIssues)
+
+	// Walrus operator method (Short Variable Declaration)
+	ghost := 99
+	fmt.Println(ghost)
+
+	// int
+	number := 42
+	fmt.Println(number)
+
+	// float64
+	pi := 3.14159
+	fmt.Println(pi)
+
+	// string
+	message := "Hello World"
+	fmt.Println(message)
+
+	// bool
+	isGoat := true
+	fmt.Println(isGoat)
+
+	messageStart := "Happy birthday! You are now"
+	age := 21
+	messageEnd := "years old!"
+	fmt.Println(messageStart, age, messageEnd)
+
+}

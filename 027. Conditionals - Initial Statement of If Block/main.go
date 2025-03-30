ae232c4194b5d0b3753b6dc23611c3a8350015e8
AbhinandAmmanamkandy <abhinandammanamkandypvt@gmail.com>
2025-03-31 02:24:49 +0530

    Conditionals - Initial Statement of If Block

---
+++ 027. Conditionals - Initial Statement of If Block/main.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// why would yo use the initial section of an if statement?
+	// To keep the code concise and is scope limited
+
+	// Initial statement of an If block
+	// if INITIAL STATEMENT; CONDITION {
+	// }
+
+	// length := getLength(email)
+	// if length < 1 {
+	// 	fmt.Println("Email is invalid")
+	// }
+
+	// or
+
+	// if length := getLength(email); length < 1 {
+	// 	fmt.Println("Email is invalid")
+	// }
+	
+
+}

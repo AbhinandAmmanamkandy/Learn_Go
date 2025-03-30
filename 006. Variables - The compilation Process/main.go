e48230cb9fe9f3885afd1d8b9d913ee281a2e4ba
AbhinandAmmanamkandy <abhinandammanamkandypvt@gmail.com>
2025-03-31 00:59:59 +0530

    Variables - The compilation Process

---
+++ 006. Variables - The compilation Process/main.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Compilation Error
+	// fmt.Println("The compiled textio server is starting"
+	fmt.Println("The compiled textio server is starting")
+}

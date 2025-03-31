8c6983419dfe64101b00fc20c6c34300433af784
AbhinandAmmanamkandy <abhinandammanamkandypvt@gmail.com>
2025-03-31 12:06:44 +0530

    Functions - Declaration Syntax

---
+++ 034. Functions - Declaration Syntax/main.go
@@ -0,0 +1,20 @@
+package main
+
+func main() {
+
+	// Declaration Syntax
+
+	// What are we talking about when we discuss 'Declaration Syntax'?
+	// The style of language used to create new variables, types, functions etc
+
+	// Go's declarations are clear, you just read them left to right
+	// x int
+	// p *int
+	// a [3]int
+
+	// f func(func(int,int)int, int) int
+	// Here we have a function named f 
+	// which takes in a function and an int as an argument and it returns an int
+	// The function argument also takes in two integer arguments and returns an integer
+
+}

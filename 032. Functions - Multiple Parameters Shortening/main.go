39b03fe4a18c0e2953144daf7918174b19b9db2c
AbhinandAmmanamkandy <abhinandammanamkandypvt@gmail.com>
2025-03-31 11:48:14 +0530

    Functions - Multiple Parameters Shortening

---
+++ 032. Functions - Multiple Parameters Shortening/main.go
@@ -0,0 +1,12 @@
+package main
+
+func main() {
+
+	// When there are multiple arguments and are of same type
+	// type only need to be declared after the last argument
+
+	// func addToDatabase(hp, damage int){}
+	// func addToDatabase(hp, damage int, name string){}
+	// func addToDatabase(hp, damage int, name string, level int){}
+
+}

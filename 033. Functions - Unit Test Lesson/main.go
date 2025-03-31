8acb2d1081d6434cded794cdfd23ed9854fa985a
AbhinandAmmanamkandy <abhinandammanamkandypvt@gmail.com>
2025-03-31 12:00:43 +0530

    Functions - Unit Test Lesson

---
+++ 033. Functions - Unit Test Lesson/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func getMonthlyPrice(tier string) int {
+	switch tier {
+	case "basic":
+		return 10000
+	case "premium":
+		return 15000
+	case "enterprise":
+		return 50000
+	default:
+		return 0
+	}
+}
+
+func main() {
+	fmt.Println(getMonthlyPrice("enterprise"))
+}
